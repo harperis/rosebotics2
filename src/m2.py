58bf22f6d631ddce66655a7253b3e99a0010a0a5
harperis <harperis@rose-hulman.edu>
2018-10-28 18:06:40 -0400

    I think I finished up the color sensor

--- src/m2.py
+++ src/m2.py
@@ -1,5 +1,5 @@
 """
-  Capstone Project.  Code written by PUT_YOUR_NAME_HERE.
+  Capstone Project.  Code written by Isaac Harper.
   Fall term, 2018-2019.
 """
 
@@ -8,30 +8,20 @@ import time
 
 
 def main():
-    # drive_until_color(1)
     test_color(1)
     """ Runs YOUR specific part of the project """
 
 
-# def drive_until_color(color):
-    # print('start')
-    # robot = rb.Snatch3rRobot()
-    # if not robot.color_sensor.get_color() == color:
-        # while True:
-            # robot.drive_system.go_straight_inches(1)
-            # break
-        # robot.drive_system.stop_moving()
-    # print('end')
-
-
 def test_color(color):
     robot = rb.Snatch3rRobot()
-    if robot.color_sensor.get_color() == color:
-        robot.drive_system.stop_moving()
-        print('True')
-    else:
-        robot.drive_system.move_for_seconds(1)
-        print('False')
+    if not robot.color_sensor.get_color() == color:
+        while True:
+            robot.drive_system.start_moving()
+            print('False')
+            if robot.color_sensor.get_color() == color:
+                break
+            robot.drive_system.stop_moving()
+            print('True')
 
 
 
