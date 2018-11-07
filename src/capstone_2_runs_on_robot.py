8c7bd7094eff42b4fe4e977a4150d235a0e9ea62
harperis <harperis@rose-hulman.edu>
2018-11-07 09:26:04 -0500

    Capstone 2 runs on robot

--- src/capstone_2_runs_on_robot.py
+++ src/capstone_2_runs_on_robot.py
@@ -10,13 +10,13 @@ It uses MQTT to RECEIVE information from a program running on the LAPTOP.
 Authors:  David Mutchler, his colleagues, and Isaac Harper.
 """
 # ------------------------------------------------------------------------------
-# TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this TODO.
+# Done: 1. PUT YOUR NAME IN THE ABOVE LINE.  Then delete this Done.
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# TODO: 2. With your instructor, review the "big picture" of laptop-robot
-# TODO:    communication, per the comment in mqtt_sender.py.
-# TODO:    Once you understand the "big picture", delete this TODO.
+# Done: 2. With your instructor, review the "big picture" of laptop-robot
+# Done:    communication, per the comment in mqtt_sender.py.
+# Done:    Once you understand the "big picture", delete this Done.
 # ------------------------------------------------------------------------------
 
 import rosebotics_new as rb
@@ -39,17 +39,17 @@ def main():
     mqtt_client = com.MqttClient(rc)
     mqtt_client.connect_to_pc()
     # --------------------------------------------------------------------------
-    # TODO: 5. Add a class for your "delegate" object that will handle messages
-    # TODO:    sent from the laptop.  Construct an instance of the class and
-    # TODO:    pass it to the MqttClient constructor above.  Augment the class
-    # TODO:    as needed for that, and also to handle the go_forward message.
-    # TODO:    Test by PRINTING, then with robot.  When OK, delete this TODO.
+    # Done: 5. Add a class for your "delegate" object that will handle messages
+    # Done:    sent from the laptop.  Construct an instance of the class and
+    # Done:    pass it to the MqttClient constructor above.  Augment the class
+    # Done:    as needed for that, and also to handle the go_forward message.
+    # Done:    Test by PRINTING, then with robot.  When OK, delete this Done.
     # --------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------
-    # TODO: 6. With your instructor, discuss why the following WHILE loop,
-    # TODO:    that appears to do nothing, is necessary.
-    # TODO:    When you understand this, delete this TODO.
+    # Done: 6. With your instructor, discuss why the following WHILE loop,
+    # Done:    that appears to do nothing, is necessary.
+    # Done:    When you understand this, delete this Done.
     # --------------------------------------------------------------------------
     while True:
         # ----------------------------------------------------------------------
@@ -76,3 +76,4 @@ class RemoteControlEtc(object):
 
 
 main()
+
